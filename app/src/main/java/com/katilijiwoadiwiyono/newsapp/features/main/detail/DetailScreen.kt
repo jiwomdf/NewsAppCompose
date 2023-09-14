1b56aad1c8c3c7870c67031160c4970c0762cb9b
katili.jiwo <mdf.jiwo@gmail.com>
2023-09-14 19:38:20 +0700

    enhance dummy data

--- app/src/main/java/com/katilijiwoadiwiyono/newsapp/features/main/detail/DetailScreen.kt
+++ app/src/main/java/com/katilijiwoadiwiyono/newsapp/features/main/detail/DetailScreen.kt
@@ -45,13 +45,17 @@ fun DetailScreenPreview() {
     DetailScreen(
         newsModel = NewsModel(
             id = 1,
-            createdAt = "",
-            contributorName = "",
-            contributorAvatar = "",
-            title = "",
-            content = "",
-            contentThumbnail = "",
-            slideshow = listOf(),
+            createdAt = "2023-09-14T04:29:04.957Z",
+            contributorName = "Hope Ward IV",
+            contributorAvatar = "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/760.jpg",
+            title = "perspiciatis maxime molestiae",
+            content = "Alias molestias recusandae molestiae labore itaque consequatur atque illum dolorem. Accusamus soluta ut neque nihil earum quae consectetur quibusdam. Tempora adipisci aut quam a impedit excepturi suscipit. Officiis cum ab. Ab quas animi hic iusto ipsum at.\\nVoluptas quibusdam perspiciatis debitis harum nobis quibusdam dolores voluptatum nobis. Ut fugit beatae corrupti maxime libero error quos dignissimos dolorem. Harum incidunt quia consectetur reprehenderit debitis veritatis voluptates repellendus reiciendis.\\nTotam recusandae corporis. Maxime consequatur a nulla accusamus. Iste sapiente vel placeat aperiam. Voluptates maiores quo tempore id praesentium itaque. Voluptatem iure cumque debitis officia iure sapiente.",
+            contentThumbnail = "https://loremflickr.com/640/480",
+            slideshow = listOf(
+                "https://loremflickr.com/640/480",
+                "https://loremflickr.com/640/480",
+                "https://loremflickr.com/640/480"
+            ),
         )
     )
 }
